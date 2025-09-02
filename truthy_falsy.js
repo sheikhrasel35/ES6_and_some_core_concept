76572d2b34b426a34c68a1a2c73642bf77253198
sheikhrasel35 <abushaidrasel@gmail.com>
2025-09-03 00:58:20 +0600

    add the truthy falsy.js

---
+++ truthy_falsy.js
@@ -0,0 +1,8 @@
+// Falsy value = false, 0, "", null, undefined, NaN
+//Truthy value = falsy value bad diye baki shob holo truthy value
+
+if (0) {
+  console.log("Ami truthy value");
+} else {
+  console.log("Ami falsy value");
+}
