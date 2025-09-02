c40e1b3dd92804c3e7bc3b42cf1f1cbfbb359063
sheikhrasel35 <abushaidrasel@gmail.com>
2025-09-02 12:38:16 +0600

    add the template string.js

---
+++ template_string.js
@@ -0,0 +1,14 @@
+// var name = "Ahashan Habib Utsho"
+
+// Ternary operator -> condition ? true : false
+var age = 25;
+var name = `Ahashan Habib
+ Utsho. My age is ${age}. I am ${age < 18 ? "not" : ""} adult.`;
+
+// console.log(name);
+
+// padStart , padEnd, repeat
+var test = "demo ";
+console.log(test.padStart(10, "-"));
+console.log(test.padEnd(10, "*"));
+console.log(test.repeat(10));
